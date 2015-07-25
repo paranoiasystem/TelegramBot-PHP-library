fd70f9d72dc6b4df7715225c7b3a4f2d67e40bcc
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-07-25 20:00:43 +0200

    Test

--- tests/TelegramBotTest.php
+++ tests/TelegramBotTest.php
@@ -6,7 +6,7 @@ class TelegramBotTest extends PHPUnit_Framework_TestCase {
  
   public function testTelegram()
   {
-    $telegramBot = new TelegramBot("106915375:AAFmuEPDrhodeWU7k4sx_uRJpzxPBMFGqfk", "paranoia_bot");
+    $telegramBot = new TelegramBot("117903737:AAHj6OYikiEbOEhcnWk0lcnBF7Jte5aJLjU", "test_libphp_bot");
     $this->assertTrue($telegramBot->test($telegramBot->getMe()));
   }
  

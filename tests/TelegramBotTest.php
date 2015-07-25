cfa24fc05762dadac97d5f29b09111c28e2ed1ce
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-07-25 19:06:23 +0200

    update

--- tests/TelegramBotTest.php
+++ tests/TelegramBotTest.php
@@ -4,7 +4,7 @@ use Telegram\TelegramBot;
  
 class TelegramBotTest extends PHPUnit_Framework_TestCase {
  
-  public function testNachHasCheese()
+  public function testTelegramBot()
   {
     $telegramBot = new TelegramBot("");
     $this->assertTrue($telegramBot->test());
